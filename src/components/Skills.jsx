773b0d953b5b45b1d8a384b93efda4675fc8ca90
Xeon <whartonaldrick@gmail.com>
2025-05-05 01:08:35 +0300

    Fix : Create Manual Library Components for SVG

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import TailwindSVG from '../assets/svg/TailwindSVG'
+import SkillsCard from './SkillsCard'
+import { TailwindSVG, ReactSVG, PytonSVG, PhpSVG, NodeSVG, LaravelSVG, JsSVG, JavaSVG, HtmlSVG, FramerSVG, CssSVG } from '../assets/svg/XeonSVG'
 
 
 const Skills = () => {
@@ -9,9 +10,17 @@ const Skills = () => {
             <p className='text-gray-400 sm:my-4 my-2 text-sm'>Web developers turn ideas into functional, user-friendly websites.</p>
             <div className='sm:p-4 sm:h-[270px] h-60 overflow-y-scroll rounded-lg'>
                 <div className='grid grid-cols-2 md:grid-cols-5 gap-4 px-10'>
-                    <div className='text-red-500 w-20'>
-                        <TailwindSVG classSVG="w-40 h-40 text-green-400"/>
-                    </div>
+                    <SkillsCard icon={<HtmlSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<CssSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<JsSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<PhpSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<JavaSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<PytonSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<TailwindSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<ReactSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<NodeSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<FramerSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard icon={<LaravelSVG classSVG='w-20 h-20' />} />
                 </div>
             </div>
         </div>
--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const SkillsCard = ({ icon, desc, title }) => {
+const SkillsCard = ({ icon }) => {
     return (
         <div className=''>
-            <div className='bg-gray-800 w-24 text-white p-4 rounded-lg'>
-                <img src={icon} alt={desc} title={title} />
+            <div className='bg-gray-800 w-max text-white p-4 rounded-lg'>
+                {icon}
             </div>
         </div>
     )
