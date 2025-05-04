6f99c514bfd5f12e6a9f190e4b17ec5b44dba410
Xeon <whartonaldrick@gmail.com>
2025-05-05 01:26:08 +0300

    Edit : Customise Skills Styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,17 +10,17 @@ const Skills = () => {
             <p className='text-gray-400 sm:my-4 my-2 text-sm'>Web developers turn ideas into functional, user-friendly websites.</p>
             <div className='sm:p-4 sm:h-[270px] h-60 overflow-y-scroll rounded-lg'>
                 <div className='grid grid-cols-2 md:grid-cols-5 gap-4 px-10'>
-                    <SkillsCard icon={<HtmlSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<CssSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<JsSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<PhpSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<JavaSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<PytonSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<TailwindSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<ReactSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<NodeSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<FramerSVG classSVG='w-20 h-20' />} />
-                    <SkillsCard icon={<LaravelSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-orange-500" icon={<HtmlSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-blue-700" icon={<CssSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-yellow-400" icon={<JsSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-purple-300 hover:text-gray-900" icon={<PhpSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-gray-100 hover:text-red-600" icon={<JavaSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-blue-600 hover:text-yellow-400" icon={<PytonSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-sky-500 hover:text-gray-900" icon={<TailwindSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-gray-600 hover:text-cyan-400" icon={<ReactSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-gray-600 hover:text-green-400" icon={<NodeSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-blue-500" icon={<FramerSVG classSVG='w-20 h-20' />} />
+                    <SkillsCard style="hover:bg-gray-100 hover:text-red-600" icon={<LaravelSVG classSVG='w-20 h-20' />} />
                 </div>
             </div>
         </div>
--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SkillsCard = ({ icon }) => {
+const SkillsCard = ({ icon, style }) => {
     return (
         <div className=''>
-            <div className='bg-gray-800 w-max text-white p-4 rounded-lg'>
+            <div className={`${style} bg-gray-800 w-max text-white p-4 rounded-lg transition-all duration-300 ease-in-out hover:scale-105`}>
                 {icon}
             </div>
         </div>
